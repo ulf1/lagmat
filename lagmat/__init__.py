32fd43e5d4579f30bd951d2bd0dccf39f0696e56
UH <554c46@gmail.com>
2018-11-22 16:20:51 +0100

    chopnan added

--- lagmat/__init__.py
+++ lagmat/__init__.py
@@ -1,1 +1,2 @@
 from .lagmat_func import lagmat
+from .chopnan import chopnan, rmnanrow
---
+++ lagmat/chopnan.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+import warnings
+
+
+def chopnan(X, lags=[], nchop=None):
+    if lags:
+        nchop = max(lags)
+    if nchop is None:
+        raise Exception("No lags=[1,2,..] nor nchop=? has been provided")
+    return X[nchop:, :]
+
+
+def rmnanrow(X):
+    """Remove all rows that contain a NaN value"""
+    warnings.warn((
+        "This function 'rmnanrow' is for demonstration purpose only. "
+        "You may risk to delete rows due to missing values "
+        "that need to be fixed during data cleansing before "
+        "feature engineering."))
+    idx = np.any(np.isnan(X), axis=1)
+    return X[np.logical_not(idx), :]
