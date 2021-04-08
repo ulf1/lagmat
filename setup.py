5748de58fa12b003e3b33fd6bec80abb7ceff499
UH <554c46@gmail.com>
2021-04-08 10:54:21 +0200

    rst for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='lagmat',
       version='0.3.0',
       description=(
           "Lagmatrix. Create array with time-lagged copies of the features"),
-      # long_description=read('README.md'),
-      # long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/lagmat',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
